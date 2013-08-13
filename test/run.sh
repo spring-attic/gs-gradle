27a3b0cf0bcd7b97634760cf7809be45367a8674
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-12 21:45:18 -0400

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 cd $(dirname $0)
+
 cd ../complete
 ./gradlew build
 ret=$?
 if [ $ret -ne 0 ]; then
-exit $ret
+  exit $ret
 fi
 rm -rf build
+
+cd ../initial
+../complete/gradlew -b ../initial/build.gradle wrapper
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+rm -rf build
+rm -rf gradle
+rm gradlew*
+
 exit
